45b8093f77d5ca59681563cc543649e6b45fe126
harshitdhar9 <143949444+harshitdhar9@users.noreply.github.com>
2024-11-14 14:16:17 +0530

    Commit

--- Hair_Tracker_AI/StreamlitApp/Disease.py
+++ Hair_Tracker_AI/StreamlitApp/Disease.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 from PIL import Image
 import numpy as np
 
-model = tf.keras.models.load_model(r'AI_Models\hair_disease_cnn_model (1).h5')
+model = tf.keras.models.load_model(r'StreamlitApp\hair_disease_cnn_model (1).h5')
 
 st.markdown("""
     <style>
